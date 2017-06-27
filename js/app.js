165997856d6d6da492c7a18249af38387c3daa60
RecursiveIteration <James.C.Woodson@gmail.com>
2017-06-26 22:37:56 -0700

    added Mount Si

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@
 var trails = [
   ['Lincoln Park', 'west', 'easy', 2, 1, 2, 'http://www.wta.org/site_images/hikes/dscn6977.jpg/@@images/4e590d0c-7479-40bd-8eb4-55b4c7ef51c3.jpeg', '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2693.982881513204!2d-122.39895978525442!3d47.529196279179644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54904139d8f5169d%3A0xe7921cfad18b2bfd!2sLincoln+Park!5e0!3m2!1sen!2sus!4v1498523071389" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', 'Located on the western edge of West Seattle, Lincoln park offers 5 miles of trails, including a loop hike with bluff and beachfront views of Puget Sound, Vashon Island, and the Olympic Mountains. The park is family-friendly, with a wading pool, playground, bathroom facilities, ball fields, and multiple trail options.'],
 
-  ['Franklin Falls', 'east', 'easy', 2, 2, 2, 'images/franklin_falls.jpg', 'https://goo.gl/maps/TBv5xjqZJwk', 'The trail to Franklin Falls combines history and natural beauty along a compact and easily accessible loop. A visit to the falls makes for a particularly enjoyable half-day\'s outing for anyone who wants to explore the Snoqualmie Pass Region but isn\'t looking for a demanding mountain hike.', '60 Hikes Within 60 Miles'],
-  [],
+  ['Franklin Falls', 'east', 'easy', 2, 2, 2, 'images/franklin_falls.jpg', 'https://goo.gl/maps/TBv5xjqZJwk', 'The trail to Franklin Falls combines history and natural beauty along a compact and easily accessible loop. A visit to the falls makes for a particularly enjoyable half-day\'s outing for anyone who wants to explore the Snoqualmie Pass Region but isn\'t looking for a demanding mountain hike.', '60 Hikes Within 60 Miles - Seattle'],
+
+  ['Mount Si', 'east', 'hard', 5, 3, 4, 'images/mount_si.jpg', 'https://goo.gl/maps/pJR8X8MW1rF2', 'Just as no tourist can claim to have seen downtown Seattle without a visit to Pike Place Market, no Seattle [can claim to be a] hiker who fails to climb Mount Si. Easy to reach, physically challenging, and capped by a commanding view, the mountain known simply as Si is like a city park crossed with a serious Cascade Mountain Peak.', '60 Hikes Within 60 Miles - Seattle'],
   [],
   [],
   [],
