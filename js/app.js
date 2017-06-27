9b77145de4bc2090d203ed041423b110cbed993a
RecursiveIteration <James.C.Woodson@gmail.com>
2017-06-26 22:54:11 -0700

    Added Mud Mountain Lake and White River Trail

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ var trails = [
   ['Franklin Falls', 'east', 'easy', 2, 2, 2, 'images/franklin_falls.jpg', 'https://goo.gl/maps/TBv5xjqZJwk', 'The trail to Franklin Falls combines history and natural beauty along a compact and easily accessible loop. A visit to the falls makes for a particularly enjoyable half-day\'s outing for anyone who wants to explore the Snoqualmie Pass Region but isn\'t looking for a demanding mountain hike.', '60 Hikes Within 60 Miles - Seattle'],
 
   ['Mount Si', 'east', 'hard', 5, 3, 4, 'images/mount_si.jpg', 'https://goo.gl/maps/pJR8X8MW1rF2', 'Just as no tourist can claim to have seen downtown Seattle without a visit to Pike Place Market, no Seattle [can claim to be a] hiker who fails to climb Mount Si. Easy to reach, physically challenging, and capped by a commanding view, the mountain known simply as Si is like a city park crossed with a serious Cascade Mountain Peak.', '60 Hikes Within 60 Miles - Seattle'],
-  [],
+
+  ['Mud Mountain Lake and White River Trail', 'south', 'hard', 4, 4, 4, 'images/mud_mountain_dam.jpg', 'https://goo.gl/maps/ctvaYHwkmjr', 'This one-of-a-kind trail follows the White River above Mud Mountain Dam, first on a high bluff and then right along the water, providing the unique opportunity to walk on a part-time lake bed. With year-round access, limited traffic, and a feel more like a riparian zone in eastern Washington than anything in the Cascades, this hike is unlike any other in the region.', '60 Hikes Within 60 Miles - Seattle'],
   [],
   [],
   [],
