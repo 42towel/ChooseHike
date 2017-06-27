d614248415dbfc98be5051531dff75982a80403a
smitzter <smittybuckler@gmail.com>
2017-06-27 11:58:30 -0700

    midstream update, JS done, need to update html still

--- js/journal.js
+++ js/journal.js
@@ -3,20 +3,43 @@
 
 var journal = document.getElementById('journal');
 var key = 'storeJournal';
+var submit = document.getElementById('createEntry');
+var journalEntryArray = retrieveEntries();
+
+if (!journalEntryArray) journalEntryArray = [];
 //constructor for Journal entries
+
+submit.addEventListener('submit', function(e) {
+  e.preventDefault();
+  while (journal.firstChild) {
+    journal.removeChild(journal.firstChild);
+  }
+  console.log("this just happened");
+  var trail = e.target.trail.value;
+  var title = e.target.title.value;
+  var entry = e.target.entry.value;
+  var myEntry = new JournalEntry(trail, title, entry);
+  journalEntryArray.unshift(myEntry);
+  for (var i=0; i < journalEntryArray.length; i++) {
+    renderEntry(journalEntryArray[i]);
+  }
+  createOrUpdateEntries(journalEntryArray);
+});
+
+
 function JournalEntry(trail, title, entry) {
   this.trail = trail;
   this.title = title;
   this.entry = entry;
 }
 
-function renderEntry() {
+function renderEntry(entry) {
   var h2Title = document.createElement('h2');
-  h2Title.textContent = this.title;
+  h2Title.textContent = entry.title;
   var h3Trail = document.createElement('h3');
-  h3Trail.textContent = this.trail;
+  h3Trail.textContent = entry.trail;
   var pEntry = document.createElement('p');
-  pEntry.textContent = this.entry;
+  pEntry.textContent = entry.entry;
   journal.appendChild(h2Title);
   journal.appendChild(h3Trail);
   journal.appendChild(pEntry);
