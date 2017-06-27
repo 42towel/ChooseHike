9c9ab82327d533c158867f2ce2c8e7b3166b44fa
smitzter <smittybuckler@gmail.com>
2017-06-27 11:03:21 -0700

    updating journal.js with crud

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-//arrray of trails
-//['name', 'region', 'difficulty', 'elevation', 'quality', 'distance', 'imgUrl', 'mapUrl', 'description', 'source']
+function TrailArray('name', 'region', 'difficulty', 'elevation', 'quality', 'distance', 'imgUrl', 'mapUrl', 'description', 'source') {
+
+}
 var trails = [
   ['Seward Park', 'south', 'easy', 2, 2, 2, 'http://www.wta.org/site_images/hikes/seward-park_marley.jpeg/@@images/9013d4c3-61b0-4c3a-a384-5af755b530c8.jpeg', < iframe src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d172388.17433190768!2d-122.42183369098669!3d47.537326982291574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3c791c08c886fbb8!2sSeward+Park!5e0!3m2!1sen!2sus!4v1498578987977" width = "600" height = "450" frameborder = "0" style = "border:0" allowfullscreen > < /iframe>, 'Located southeast from downtown Seattle, Seward Park resides on the Bailey Peninsula. The park is home to a variety of educational, recreational, and cultural activities. Visitors can enjoy a 2.4 mile bike and walking loop, an amphitheater, a native plant garden, an art studio, sheltered picnic areas, slightly more challenging hiking trails, a swimming beach, and more.' ],
   ['Lincoln Park', 'west', 'easy', 2, 1, 2, 'http://www.wta.org/site_images/hikes/dscn6977.jpg/@@images/4e590d0c-7479-40bd-8eb4-55b4c7ef51c3.jpeg', '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2693.982881513204!2d-122.39895978525442!3d47.529196279179644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54904139d8f5169d%3A0xe7921cfad18b2bfd!2sLincoln+Park!5e0!3m2!1sen!2sus!4v1498523071389" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', 'Located on the western edge of West Seattle, Lincoln park offers 5 miles of trails, including a loop hike with bluff and beachfront views of Puget Sound, Vashon Island, and the Olympic Mountains. The park is family-friendly, with a wading pool, playground, bathroom facilities, ball fields, and multiple trail options.'],
--- js/journal.js
+++ js/journal.js
@@ -1,8 +1,36 @@
 'use strict';
 
+
+var journal = document.getElementById('journal');
+var key = 'storeJournal';
 //constructor for Journal entries
-function JournalEntry (trail, title, entry) {
+function JournalEntry(trail, title, entry) {
   this.trail = trail;
   this.title = title;
   this.entry = entry;
+}
+
+function renderEntry() {
+  var h2Title = document.createElement('h2');
+  h2Title.textContent = this.title;
+  var h3Trail = document.createElement('h3');
+  h3Trail.textContent = this.trail;
+  var pEntry = document.createElement('p');
+  pEntry.textContent = this.entry;
+  journal.appendChild(h2Title);
+  journal.appendChild(h3Trail);
+  journal.appendChild(pEntry);
+}
+
+
+function createOrUpdateEntries(entries) {
+  var stringifiedEntries = JSON.stringify(entries);
+  localStorage.setItem(key, stringifiedEntries);
+  return stringifiedEntries;
+}
+
+function retrieveEntries() {
+  var stringifiedEntries = localStorage.getItem(key);
+  var entries = JSON.parse(stringifiedEntries);
+  return entries;
 }
