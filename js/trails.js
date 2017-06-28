fc40dfedd176cc04f3e27367936cfc74ed18d120
smitzter <smittybuckler@gmail.com>
2017-06-28 10:15:05 -0700

    On Lincoln Park iframe push to trails page.

--- js/trails.js
+++ js/trails.js
@@ -3,7 +3,7 @@ var key = 'selectionCriteria';
 var selectionCriteria = retrieveSelectionCriteria();
 var imgDiv =  document.getElementById('img');
 var regionDiv = document.getElementById('region');
-var mapDiv = document.getElementById('map');
+var mapDiv = document.getElementById('iframe');
 var diffDiv = document.getElementById('difficulty');
 var trailDiv = document.getElementById('trailName');
 
@@ -16,7 +16,7 @@ var trails = [
     quality: 2,
     distance: 2,
     imgUrl: 'images/seward_park.jpg',
-    map: 'map placeholder',
+    mapUrl: 'map placeholder',
     description: 'Located southeast from downtown Seattle, Seward Park resides on the Bailey Peninsula. The park is home to a variety of educational, recreational, and cultural activities. Visitors can enjoy a 2.4 mile bike and walking loop, an amphitheater, a native plant garden, an art studio, sheltered picnic areas, slightly more challenging hiking trails, a swimming beach, and more.',
     source: 'source placeholder'
   },
@@ -28,7 +28,8 @@ var trails = [
     quality: 1,
     distance: 2,
     imgUrl:  'images/lincoln_park.jpg',
-    map: 'https://goo.gl/maps/yAYQJDnsr8S2',
+    mapUrl:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2693.982881513204!2d-122.39895978525442!3d47.529196279179644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54904139d8f5169d%3A0xe7921cfad18b2bfd!2sLincoln+Park!5e0!3m2!1sen!2sus!4v1498519465594",
+    //  'https://goo.gl/maps/yAYQJDnsr8S2',
     description: 'Located on the western edge of West Seattle, Lincoln park offers 5 miles of trails, including a loop hike with bluff and beachfront views of Puget Sound, Vashon Island, and the Olympic Mountains. The park is family-friendly, with a wading pool, playground, bathroom facilities, ball fields, and multiple trail options.',
     source: 'source placeholder'
   },
@@ -40,7 +41,7 @@ var trails = [
     quality: 2,
     distance: 2,
     imgUrl: 'images/franklin_falls.jpg',
-    map: 'https://goo.gl/maps/TBv5xjqZJwk',
+    mapUrl: 'https://goo.gl/maps/TBv5xjqZJwk',
     description: 'The trail to Franklin Falls combines history and natural beauty along a compact and easily accessible loop. A visit to the falls makes for a particularly enjoyable half-day\'s outing for anyone who wants to explore the Snoqualmie Pass Region but isn\'t looking for a demanding mountain hike.',
     source: '60 Hikes Within 60 Miles - Seattle'
   },
@@ -52,7 +53,7 @@ var trails = [
     quality: 3,
     distance: 4,
     imgUrl: 'images/mount_si.jpg',
-    map: 'https://goo.gl/maps/pJR8X8MW1rF2',
+    mapUrl: 'https://goo.gl/maps/pJR8X8MW1rF2',
     description: 'Just as no tourist can claim to have seen downtown Seattle without a visit to Pike Place Market, no Seattle [can claim to be a] hiker who fails to climb Mount Si. Easy to reach, physically challenging, and capped by a commanding view, the mountain known simply as Si is like a city park crossed with a serious Cascade Mountain Peak.',
     source: '60 Hikes Within 60 Miles - Seattle'
   },
@@ -64,7 +65,7 @@ var trails = [
     quality: 4,
     distance: 4,
     imgUrl: 'images/mud_mountain_dam.jpg',
-    map: 'https://goo.gl/maps/ctvaYHwkmjr',
+    mapUrl: 'https://goo.gl/maps/ctvaYHwkmjr',
     description: 'This one-of-a-kind trail follows the White River above Mud Mountain Dam, first on a high bluff and then right along the water, providing the unique opportunity to walk on a part-time lake bed. With year-round access, limited traffic, and a feel more like a riparian zone in eastern Washington than anything in the Cascades, this hike is unlike any other in the region.',
     source: '60 Hikes Within 60 Miles - Seattle',
   },
@@ -76,7 +77,7 @@ var trails = [
     quality: 1,
     distance: 2,
     imgUrl:  'images/carkeek_park.jpg',
-    map:'placeholder',
+    mapUrl:'placeholder',
     description: 'Carkeek Park is located in northwest Seattle, between 3rd Ave NW and Puget Sound. The park includes all of Piper\'s Creek (sometimes written without the apostrophe) plus the adjoining side hills and some side streams, plus a beach area. Park trails can be accessed from several points, and there is an access road that leads to a parking area near the beach. Park entrance is free, and official hours are daily, 6 a.m. to 10 p.m.'
   },
   {
@@ -144,8 +145,8 @@ function renderTrail(trail) {
   diffEl.textContent = trail.difficulty;
   diffDiv.appendChild(diffEl);
 
-  var mapEl = document.createElement('a');
-  mapEl.setAttribute('href', trail.mapUrl);
+  var mapEl = document.createElement('iframe');
+  mapEl.setAttribute('src', trail.mapUrl);
   mapEl.textContent= 'click for map';
   mapDiv.appendChild(mapEl);
 
@@ -160,6 +161,10 @@ function retrieveSelectionCriteria() {
   selectionCriteria = JSON.parse(stringifiedSelectionCriteria);
   return selectionCriteria;
 }
+
+// <div class="embed-responsive embed-responsive-16by9">
+// <iframe class="embed-responsive-item" src="//www.youtube.com/embed/wMuNjnNyaiA" allowfullscreen></iframe>
+// </div>
 
 //turn array to objects
 //event listener
